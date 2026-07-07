2d0f8dc45667adc7a2a89846751908ed16f1bfa7
Bil4l7 <109904134+biil4l7@users.noreply.github.com>
2026-07-07 14:22:57 +0300

    Refactor logger setup by cleaning up code
    Removed comments and unnecessary imports from logger setup.

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,26 +1,18 @@
 import logging
 import os
-from datetime import datetime
 
 def setup_logger(name='signal_bot', log_file='logs/bot.log'):
-    """Setup logger configuration"""
-    
-    # Create logs directory if it doesn't exist
     os.makedirs('logs', exist_ok=True)
     
-    # Configure logging
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
     
-    # File handler
     file_handler = logging.FileHandler(log_file)
     file_handler.setLevel(logging.INFO)
     
-    # Console handler (for Railway logs)
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
     
-    # Formatter
     formatter = logging.Formatter(
         '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S'
@@ -28,11 +20,9 @@ def setup_logger(name='signal_bot', log_file='logs/bot.log'):
     file_handler.setFormatter(formatter)
     console_handler.setFormatter(formatter)
     
-    # Add handlers
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
     
     return logger
 
-# Default logger instance
 logger = setup_logger()
